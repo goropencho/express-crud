19a2cc8ae2f8e616594f26d1620767ad49d25b77
Gaurav Panchal <gkp.125@gmail.com>
2023-12-18 20:28:34 +0530

    Bug Fixes

--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -1,52 +1,53 @@
 import express from 'express';
 const users = express.Router();
 const userView = require('../views/users.views');
-import {createUser, deleteUser, selectUser, updateUser} from '../validations/users.validations';
-import { validationResult } from 'express-validator';
+import { createUser, deleteUser, selectUser, updateUser } from '../validations/users.validations';
+import { validationResult,matchedData } from 'express-validator';
 
-users.get('/', (req, res) =>  userView.getAllUser(req, res));
-users.get('/:id', selectUser, (req, res) =>  {
+users.get('/', (req, res) => userView.getAllUser(req, res));
+users.get('/:id', selectUser, (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            success:false,
-            errors: errors.array()
-        })
-    }
-    return userView.getUser(req.params.id, res)});
-
-users.post('/', createUser ,(req:any,res:any) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json({
             success: false,
-            errors: errors.array()
+            errors: errors.array().map(err => { return {message: err.msg }})
         })
     }
-    return userView.createUser(req.body,res);
+    return userView.getUser(req.params.id, res)
+});
+
+users.post('/', createUser, (req: any, res: any) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            success: false,
+            errors: errors.array().map(err => { return {message: err.msg }})
+        })
+    }
+    return userView.createUser(matchedData(req, {includeOptionals: false}), res);
 })
 
-users.put('/:id', updateUser,(req:any,res:any) => {
+users.put('/:id', updateUser, (req: any, res: any) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json({
-            success:false,
-            errors: errors.array()
+            success: false,
+            errors: errors.array().map(err => { return {message: err.msg }})
         })
     }
-    return userView.updateUser(req.params.id,req.body, res)
+    return userView.updateUser(req.params.id, matchedData(req,{includeOptionals: false}), res)
 })
 
 
 users.delete('/:id', deleteUser, (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json({
-            success:false,
-            errors: errors.array()
+            success: false,
+            errors: errors.array().map(err => { return {message: err.msg }})
         })
     }
-    return userView.deleteUser(req.params.id,res);
+    return userView.deleteUser(req.params.id, res);
 })
 
 
--- validations/users.validations.ts
+++ validations/users.validations.ts
@@ -4,14 +4,14 @@ import { body, param } from "express-validator";
 
 export const createUser = [
     body("username").exists().withMessage("username is missing").isString().notEmpty().withMessage("username cannot be empty"),
-    body("age").exists().withMessage("age is missing").isNumeric().withMessage("age must be a number"),
+    body("age").exists().withMessage("age is missing").isInt().withMessage("age must be a number"),
     body("hobbies").exists().isArray().withMessage("Shoud be an array of strings"),
 ]
 
 export const updateUser = [
     param("id").exists().isUUID(),
     body("username").isString().optional(),
-    body("age").isNumeric().optional(),
+    body("age").isInt().optional(),
     body("hobbies").isArray().optional(),
 ]
 
@@ -20,5 +20,5 @@ export const deleteUser = [
 ]
 
 export const selectUser = [
-    param("id").exists().isUUID(),
+    param("id").exists().isUUID().withMessage("Invalid UUID"),
 ]
